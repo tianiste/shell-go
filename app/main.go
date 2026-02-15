c1e50fe4af6ff12d03b191f3af2ecec80aacc206
Tian Istenič <tian.istenic34@gmail.com>
2026-02-15 19:39:13 +0100

    third stage

--- app/main.go
+++ app/main.go
@@ -7,12 +7,18 @@ import (
 )
 
 func main() {
+	commands := map[string]interface{}{}
+	reader := bufio.NewScanner(os.Stdin)
 	fmt.Print("$ ")
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "error reading input:", err)
-		os.Exit(1)
-	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 
+	for reader.Scan() {
+		text := reader.Text()
+		if command, exists := commands[text]; !exists {
+			fmt.Println(text + ": command not found")
+		} else if exists {
+			fmt.Println(command)
+		}
+		fmt.Print("$ ")
+
+	}
 }
