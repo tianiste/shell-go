cb674cc77f470e09314271e992255635e17d6657
Tian Istenič <tian.istenic34@gmail.com>
2026-02-15 19:20:50 +0100

    second stage

--- app/main.go
+++ app/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Print("$ ")
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error reading input:", err)
+		os.Exit(1)
+	}
+	fmt.Println(command[:len(command)-1] + ": command not found")
+
 }
