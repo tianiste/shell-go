2dd7dd4f812216b77d54f827f414e48ca8d15ba2
Tian Istenič <tian.istenic34@gmail.com>
2026-03-07 20:40:43 +0100

    feat: added support for shell builtin commands

--- app/pipeline.go
+++ app/pipeline.go
@@ -22,6 +22,11 @@ func splitPipeline(args []string) (cmd1 []string, cmd2 []string) {
 	return args, nil
 }
 
+func isBuiltin(cmdName string) bool {
+	_, exists := commands[cmdName]
+	return exists
+}
+
 func executePipeline(commands []string) error {
 	input1, input2 := splitPipeline(commands)
 
@@ -30,35 +35,56 @@ func executePipeline(commands []string) error {
 		return err
 	}
 
-	cmd1, err := lookupCommand(input1[0], input1[1:])
-	if err != nil {
-		return err
+	cmd1Name := input1[0]
+	cmd1Args := input1[1:]
+	cmd2Name := input2[0]
+	cmd2Args := input2[1:]
+
+	cmd1IsBuiltin := isBuiltin(cmd1Name)
+	cmd2IsBuiltin := isBuiltin(cmd2Name)
+
+	// Start first command (either builtin or external)
+	if cmd1IsBuiltin {
+		// Run builtin with stdout redirected to pipe
+		originalStdout := os.Stdout
+		os.Stdout = writeEnd
+		runCommand(cmd1Name, cmd1Args)
+		os.Stdout = originalStdout
+		writeEnd.Close()
+	} else {
+		// Start external command
+		cmd1, err := lookupCommand(cmd1Name, cmd1Args)
+		if err != nil {
+			return err
+		}
+		cmd1.Stdout = writeEnd
+		cmd1.Stderr = os.Stderr
+		cmd1.Start()
+		writeEnd.Close()
+		defer cmd1.Wait()
 	}
 
-	cmd2, err := lookupCommand(input2[0], input2[1:])
-	if err != nil {
-		return err
+	// Start second command (either builtin or external)
+	if cmd2IsBuiltin {
+		// Run builtin with stdin redirected from pipe
+		originalStdin := os.Stdin
+		os.Stdin = readEnd
+		runCommand(cmd2Name, cmd2Args)
+		os.Stdin = originalStdin
+		readEnd.Close()
+	} else {
+		// Start external command
+		cmd2, err := lookupCommand(cmd2Name, cmd2Args)
+		if err != nil {
+			return err
+		}
+		cmd2.Stdin = readEnd
+		cmd2.Stdout = os.Stdout
+		cmd2.Stderr = os.Stderr
+		cmd2.Start()
+		readEnd.Close()
+		cmd2.Wait()
 	}
-
-	// start the pipe first command writes to it, second command reads from it
-	cmd1.Stdout = writeEnd
-	cmd1.Stderr = os.Stderr
-	cmd2.Stdin = readEnd
-	cmd2.Stdout = os.Stdout
-	cmd2.Stderr = os.Stderr
-
-	// Start both commands running at the same time
-	cmd1.Start()
-	cmd2.Start()
-
-	// Close our copies of the pipe ends so the commands know when to stop
-	// (without this, cmd2 would wait forever for more input)
-	writeEnd.Close()
-	readEnd.Close()
-
-	// Wait for both commands to finish their work
-	cmd1.Wait()
-	cmd2.Wait()
 
 	return nil
 }
